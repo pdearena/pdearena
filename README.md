199e4e6da95f8d47cda5d184a880f899b73b36fe
rejuvyesh <mail@rejuvyesh.com>
2022-09-28 23:25:39 +0000

    basic readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PDEArena
 
+Tested on PyTorch 1.12.
+
 - `scripts/generate.py` for generating Navier Stokes 2D data
 - `scripts/train.py` for training non conditioned models
 - `scripts/cond_train.py` for training conditioned models
--- scripts/compute_normalization.py
+++ scripts/compute_normalization.py
@@ -40,46 +40,6 @@ def compute_normstats_weather(datapath):
     }
     return stats
 
-def compute_normstats_maxwell(datapath):
-    filelist = glob.glob(os.path.join(datapath, "*train*.h5"))
-
-    d_field_st = {"sum": None, "sqsum": None, "count": 0}
-    h_field_st = {"sum": None, "sqsum": None, "count": 0}
-    for file in tqdm(filelist):
-        with h5py.File(file, "r") as f:
-            data = f["train"]
-            d_field = data["d_field"]
-            h_field = data["h_field"]
-            # B T X Y Z C
-            if d_field_st["sum"] is None:
-                d_field_st["sum"] = np.sum(d_field, axis=(0,1))
-                d_field_st["sqsum"] = np.sum(np.asarray(d_field)**2, axis=(0,1))
-                d_field_st["count"] = np.prod(d_field.shape[0:2])
-            else:
-                d_field_st["sum"] += np.sum(d_field, axis=(0,1))
-                d_field_st["sqsum"] += np.sum(np.asarray(d_field)**2, axis=(0,1))
-                d_field_st["count"] += np.prod(d_field.shape[0:2])
-
-            if h_field_st["sum"] is None:
-                h_field_st["sum"] = np.sum(h_field, axis=(0, 1))
-                h_field_st["sqsum"] = np.sum(np.asarray(h_field)**2, axis=(0, 1))
-                h_field_st["count"] = np.prod(h_field.shape[0:2])
-            else:
-                h_field_st["sum"] += np.sum(h_field, axis=(0,1))
-                h_field_st["sqsum"] += np.sum(np.asarray(h_field)**2, axis=(0, 1))
-                h_field_st["count"] += np.prod(h_field.shape[0:2])
-
-    stats = {
-        "d_field": {
-            "mean": torch.tensor(d_field_st["sum"] / d_field_st["count"]),
-            "std": torch.tensor(np.sqrt(d_field_st["sqsum"] / d_field_st["count"] - d_field_st["sum"]**2 / d_field_st["count"]**2)),
-        },
-        "h_field": {
-            "mean": torch.tensor(h_field_st["sum"] / h_field_st["count"]),
-            "std": torch.tensor(np.sqrt(h_field_st["sqsum"] / h_field_st["count"] - h_field_st["sum"]**2 / h_field_st["count"]**2)),
-        }
-    }            
-    return stats
 
 @click.command()
 @click.argument("datapath", type=click.Path(exists=True))
@@ -87,8 +47,8 @@ def compute_normstats_maxwell(datapath):
 def main(datapath, dataset):
     if dataset == "weather":
         stats = compute_normstats_weather(datapath)
-    elif dataset == "maxwell":
-        stats = compute_normstats_maxwell(datapath)
+    else:
+        raise NotImplementedError(f"Dataset {dataset} not implemented.")
     print(stats)
     torch.save(stats, os.path.join(datapath, "normstats.pt"))
     return stats
