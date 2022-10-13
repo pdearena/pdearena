a92e202d1ee38d5b586b4742fbf61de7c34f19ce
rejuvyesh <mail@rejuvyesh.com>
2022-10-13 21:07:53 +0000

    better setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+extras = {
+    "datagen": ["phiflow", "joblib"]
+}
+
 setup(
     name="pdearena",
     description="PyTorch library for PDE Surrogate Benchmarking",
     install_requires=[],
+    extras_require=extras,
     packages=find_packages(),
-    version="0.0.1",
-    author="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    version="0.1.0",
+    author="Jayesh K. Gupta <jayesh.gupta@microsoft.com>, Johannes Brandstetter <johannesb@microsoft.com>",
+    python_requires=">=3.6",
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],    
 )
