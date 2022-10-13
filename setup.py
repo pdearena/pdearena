7a3949dff3c227f54ebd1efee7a5fc1f95b03d81
rejuvyesh <mail@rejuvyesh.com>
2022-10-13 22:15:36 +0000

    update requirements for datagen

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 extras = {
-    "datagen": ["phiflow", "joblib"]
+    "datagen": ["phiflow", "joblib", "juliapkg"],
 }
 
 setup(
