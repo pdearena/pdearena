5291d4dd91a87413a084205861ad1f77313a5680
rejuvyesh <mail@rejuvyesh.com>
2022-10-28 18:21:00 +0000

    add dependencies

--- setup.py
+++ setup.py
@@ -6,13 +6,32 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 extras = {
-    "datagen": ["phiflow", "joblib", "juliapkg"],
+    "datagen": [
+        "phiflow",
+        "joblib",
+        "juliapkg",
+        "tqdm",
+    ],
 }
+
+base_requires = [
+    "pytorch-lightning>=1.7",
+    "numpy",
+    "xarray",
+    "h5py",
+    "click",
+    "torch>=1.12",
+    "torchdata",
+    "matplotlib",
+    "jsonargparse",
+    "omegaconf",
+    "tensorboard"
+]
 
 setup(
     name="pdearena",
     description="PyTorch library for PDE Surrogate Benchmarking",
-    install_requires=[],
+    install_requires=base_requires,
     extras_require=extras,
     packages=find_packages(),
     long_description=long_description,
@@ -22,10 +41,16 @@ setup(
     author="Jayesh K. Gupta <jayesh.gupta@microsoft.com>, Johannes Brandstetter <johannesb@microsoft.com>",
     python_requires=">=3.6",
     zip_safe=True,
+    project_urls={
+        "Documentation": "https://microsoft.github.io/pdearena",
+        "Source code": "https://github.com/microsoft/pdearena",
+        "Bug Tracker": "https://github.com/microsoft/pdearena/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
-    ],    
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
 )
